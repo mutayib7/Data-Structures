cbdea694e074c2ae0af2154969c35321366ffc71
mutayib7 <133779249+mutayib7@users.noreply.github.com>
2024-06-05 22:24:26 +0530

    Update lowuppcse228.c

--- lowuppcse228.c
+++ lowuppcse228.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {
-char str1[10];
-char str2[10];
+char str[10];
+
 
 printf("enter a string in lowercase");
-gets(str1);
-int i=0,j=0;
-while(str1[i]!='\0')
+gets(str);
+int i=0;
+while(str[i]!='\0')
 {
-    str2[j]=str1[i]-32;
+    str[i]=str[i]-32;
     i++;
-    j++;
+    
    
 }
-puts(str2);
+puts(str);
 }
