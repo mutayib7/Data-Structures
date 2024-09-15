1c0056290fe26ea5db41224bc503706441ba7f23
mutayib7 <133779249+mutayib7@users.noreply.github.com>
2024-09-15 15:10:47 +0530

    Update

---
+++ cirstudents.c
@@ -0,0 +1,66 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+struct node 
+{
+    char name[50];
+    int age;
+    
+    int marks;
+    struct node *next;
+};
+struct node *head = NULL, *tail = NULL;
+
+void create_ll(struct node *temp)
+{
+
+    int n;
+    printf("Enter number of students whose info is to be stored\n");
+    scanf("%d",&n);
+    for(int i = 0; i < n; i++)
+    {
+        temp = (struct node*)malloc(sizeof(struct node));
+        temp->next=NULL;
+        printf("Enter student name\n");
+        scanf("%s",temp->name);
+        printf("Enter age\n");
+        scanf("%d",&temp->age);
+        printf("Enter marks\n");
+        scanf("%d",&temp->marks);
+
+        if(head == NULL)
+        {
+            head  = temp;
+            tail  = temp;
+            tail->next=head;
+        }
+        else
+        {
+            tail->next = temp;
+            tail = temp;
+            tail->next=head;
+        }
+        
+    }
+}
+
+void view_ll(struct node *a)
+{
+    struct node *p = head;
+    while(p->next!=head)
+    {
+        if(p->marks<100){
+        printf("student details are:\n");
+        printf("Name: %s\n",p->name);
+        printf("Age: %d\n", p->age);
+        printf("Marks: %d\n",p->marks);
+        }
+        p=p->next;
+    }
+}
+int main()
+{
+    struct node *temp;
+    create_ll(temp);
+    view_ll(temp);
+}
